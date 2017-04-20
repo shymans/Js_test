fd84174337113536d59bf94ab450bbde2ffc5874
Austin Shymanski <austin.shymanski@gmail.com>
2017-04-20 13:42:29 -0400

    changes shape and position

--- scripts.js
+++ scripts.js
@@ -21,6 +21,17 @@ function comeBack() {
   time=time*5000;
 
   setTimeout(function() {
+    if (Math.random() > 0.5) {
+      document.getElementById("box").style.borderRadius="100px";
+    } else {
+      document.getElementById("box").style.borderRadius="0";
+    }
+    var top=Math.random();
+    top=top*500;
+    var left=Math.random();
+    left=left*800;
+    document.getElementById("box").style.top=top+"px";
+    document.getElementById("box").style.left=left+"px";
     document.getElementById("box").style.backgroundColor=getRandomColor();
     document.getElementById("box").style.display="block";
     createdTime=Date.now();
