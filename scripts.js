cf03a4671683e4a08eac6a3b9e2f97adf313105d
Austin Shymanski <austin.shymanski@gmail.com>
2017-04-20 13:26:43 -0400

    all functions in external file

--- scripts.js
+++ scripts.js
@@ -7,12 +7,31 @@ function dissapear() {
 	document.getElementById("box").style.display="none";
 }
 
+function getRandomColor() {
+  var letters = '0123456789ABCDEF'.split('');
+  var color = '#';
+  for (var i=0; i<6; i++) {
+    color += letters[Math.round(Math.random() * 15)];
+  }
+  return color;
+}
+
 function comeBack() {
   var time=Math.random();
   time=time*5000;
 
   setTimeout(function() {
+    document.getElementById("box").style.backgroundColor=getRandomColor();
     document.getElementById("box").style.display="block";
     createdTime=Date.now();
   }, time);
 }
+
+function clickMe() {
+  document.getElementById("box").onclick=function() {
+    dissapear();
+
+    comeBack();
+    }
+}
+
